95207eea2a9cc416174826717165b2d3854e0b43
abhijith-vynzora <abhijithp.vynzora@gmail.com>
2026-05-29 11:19:40 +0530

    date field added in contact.html

--- athidhi_holidays_app/models.py
+++ athidhi_holidays_app/models.py
@@ -13,6 +13,7 @@ class ContactModel(models.Model):
     phone = models.CharField(max_length=20, blank=True, null=True)
     email = models.EmailField(blank=True, null=True)
     message = models.TextField(blank=True, null=True)
+    created_at = models.DateTimeField(default=now, blank=True, null=True)
 
     def __str__(self):
         return self.name if self.name else "Unnamed Contact"
